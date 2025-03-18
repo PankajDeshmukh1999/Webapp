a7789bb4218cd4e9ab447080228e1d5c3bf5b0f4
PankajDeshmukh1999 <pankajdeshmukh1999@gmail.com>
2025-03-18 15:58:16 +0530

    Caching and rate limit implimented

--- eShopAPIGateway/Program.cs
+++ eShopAPIGateway/Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.Extensions.Configuration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional:false, reloadOnChange:true);
-builder.Services.AddOcelot();
+
+//builder.Services.AddOcelot();// normal API
+
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(setting =>
+setting.WithDictionaryHandle()); // to handle request in time using cache manager for quick response
+
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
