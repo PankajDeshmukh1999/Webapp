8b8a81fb47ec133f6a941a3a6d3c04855eec5d5a
PankajDeshmukh1999 <pankajdeshmukh1999@gmail.com>
2025-04-19 10:15:15 +0530

    Added submodule student service

---
+++ SubModuleOperation/Class1.cs
@@ -0,0 +1,13 @@
+﻿using StudentManagement.Data;
+using StudentManagement.Models;
+
+namespace SubModuleOperation
+{
+    public class Class1
+    {
+        Student s = new Student();
+
+        private readonly IStudentRepository? studentRepository;
+
+    }
+}
